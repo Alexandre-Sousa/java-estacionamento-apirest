9914f5dc415d8b39d2e60b9e2f1adccbe3c74351
DESKTOP-89B4AMI\alexa <alexandresousa.eng@gmail.com>
2025-07-15 00:22:13 -0300

    null

--- README.md
+++
@@ -1,2 +0,0 @@
-# java-estacionamento-apirest
-Amostra de API REST de um estacionamento, usando Spring Boot.
--- src/main/java/com/catenarit/demoparkapi/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.catenarit.demoparkapi.repository;
-
-import com.catenarit.demoparkapi.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-
-}
