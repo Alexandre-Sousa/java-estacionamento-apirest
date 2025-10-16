b899a1b9cbb3b27c1649fa379bbd816e9e893847
DESKTOP-89B4AMI\alexa <alexandresousa.eng@gmail.com>
2025-10-15 23:00:14 -0300

    Diversos recursos.

--- src/main/java/com/catenarit/demoparkapi/service/UsuarioService.java
+++ src/main/java/com/catenarit/demoparkapi/service/UsuarioService.java
@@ -1,9 +1,13 @@
 package com.catenarit.demoparkapi.service;
 
 import com.catenarit.demoparkapi.entity.Usuario;
+import com.catenarit.demoparkapi.exception.EntityNotFoundException;
+import com.catenarit.demoparkapi.exception.PasswordInvalidException;
+import com.catenarit.demoparkapi.exception.UsernameUniqueViolationException;
 import com.catenarit.demoparkapi.repository.UsuarioRepository;
 //import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,20 +21,31 @@ public class UsuarioService {
 
     @Transactional
     public Usuario salvar(Usuario usuario) {
-        return usuarioRepository.save(usuario);
+        try {
+            return usuarioRepository.save(usuario);
+        } catch (DataIntegrityViolationException ex) {
+            throw new UsernameUniqueViolationException(String.format("Username {%s} já cadastrado.", usuario.getUsername()));
+        }
+
     }
 
     @Transactional(readOnly = true)
     public Usuario buscarPorId(Long id) {
         return usuarioRepository.findById(id).orElseThrow(
-                () -> new RuntimeException("Usuário não encontrado.")
+                () -> new EntityNotFoundException(String.format("Usuário {id=%s} não encontrado.", id))
         );
     }
 
     @Transactional
-    public Usuario editarSenha(Long id, String password) {
+    public Usuario editarSenha(Long id, String senhaAtual, String novaSenha, String confirmaSenha) {
+        if (!novaSenha.equals(confirmaSenha)) {
+            throw new PasswordInvalidException("Nova senha não confere com confirmação de senha.");
+        }
         Usuario user = buscarPorId(id);
-        user.setPassword(password);
+        if (!user.getPassword().equals(senhaAtual)) {
+            throw new PasswordInvalidException("Sua senha não confere.");
+        }
+        user.setPassword(novaSenha);
         return user;
     }
 
