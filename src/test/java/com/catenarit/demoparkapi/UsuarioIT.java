15f01dcef5b7558c3e40e5afc77d93a2e448c369
DESKTOP-89B4AMI\alexa <alexandresousa.eng@gmail.com>
2025-10-22 00:04:34 -0300

    test: usuário já existente Criado teste para usuário já existente na base. EST-8

--- src/test/java/com/catenarit/demoparkapi/UsuarioIT.java
+++ src/test/java/com/catenarit/demoparkapi/UsuarioIT.java
@@ -139,4 +139,22 @@ public class UsuarioIT {
 
     }
 
+    @Test
+    public void createUsuario_ComUsernameRepetido_RetornarErrorMessageComStatus409() {
+
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri("api/v1/usuarios")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioCrateDto("ana@email.com", "123456"))
+                .exchange()
+                .expectStatus().isEqualTo(409)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(409);
+
+    }
+
 }
